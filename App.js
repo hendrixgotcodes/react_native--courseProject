8b9c66f82b2cb982523f11f773d86b9775ab7637
Samuel Opoku Asare <asare11samuel@gmail.com>
2021-03-12 16:53:13 +0000

    Added image, statusbar and safearea components

--- App.js
+++ App.js
@@ -1,25 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 
 export default function App() {
 
   let x = 1;
-  console.log("App executed");
 
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native!</Text>
+    <SafeAreaView style={styles.container}>
+      {/* <Image source={require('./assets/adaptive-icon.png')}/> */}
+      <Image style={styles.image} source={{
+        width: 200,
+        height: 300,
+        uri: "https://picsum.photos/200/300"}} />
+      <Text style={styles.button} >Hello React Native!</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#4287f5',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image:{
+    marginBottom: 10,
+    borderWidth: 5,
+    borderColor: '#fff',
+    borderRadius: 8
+  },
+  button:{
+    borderRadius: 25,
+    backgroundColor: '#fff',
+    padding: 16
+  }
 });
