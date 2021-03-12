20d2f01f8204870b28801e300951820c097e7f2b
Samuel Opoku Asare <asare11samuel@gmail.com>
2021-03-12 15:07:25 +0000

    Generated resources

--- App.js
+++ App.js
@@ -3,9 +3,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  let x = 1;
+  console.log("App executed");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello React Native!</Text>
       <StatusBar style="auto" />
     </View>
   );
